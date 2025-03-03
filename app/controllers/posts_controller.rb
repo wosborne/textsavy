bb8cbe777267b0581f7032522cc620d85c06d8dd
William Osborne <wosborne@live.co.uk>
2025-03-03 21:09:04 +0000

    Fix rubocop offences

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,6 @@ class PostsController < ApplicationController
   end
 
   def update
-
     if @post.update(post_params)
       redirect_to post_path(@post), notice: "Post was successfully updated."
     else
