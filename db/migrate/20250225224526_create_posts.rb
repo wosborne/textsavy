8709bf867a37f0f2255c322c7c516b33bccd32e6
William Osborne <wosborne@live.co.uk>
2025-03-04 07:26:58 +0000

    Remove user association from posts

--- db/migrate/20250225224526_create_posts.rb
+++ db/migrate/20250225224526_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.integer :user_id
       t.boolean :archived, default: false
 
       t.timestamps
